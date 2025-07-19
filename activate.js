2f82e0b403099db261159105379b34ad1a35ba7a
CluelessCatBurger <acct_cl@mail.rvvm.ing>
2025-07-19 15:23:42 -0300

    Fix variable declaration order

--- activate.js
+++ activate.js
@@ -4,12 +4,13 @@ var topmost = null;
 var order = -1;
 
 for (let win of workspace.stackingOrder) {
-  if (win.layer != 2 || win.minimized || win.deleted || (win.desktops.length != 0 && win.desktops.indexOf(workspace.currentDesktop) == -1)) continue;
-  if (x >= workspace.workspaceWidth || y >= workspace.workspaceHeight || x + width <= 0 || y + height <= 0) continue;
   const x = win.frameGeometry.x;
   const y = win.frameGeometry.y;
   const width = win.frameGeometry.width;
   const height = win.frameGeometry.height;
+
+  if (win.layer != 2 || win.minimized || win.deleted || (win.desktops.length != 0 && win.desktops.indexOf(workspace.currentDesktop) == -1)) continue;
+  if (x >= workspace.workspaceWidth || y >= workspace.workspaceHeight || x + width <= 0 || y + height <= 0) continue;
 
   if (win.stackingOrder > order) {
     topmost = win;
